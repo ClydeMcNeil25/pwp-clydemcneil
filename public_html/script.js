2c6c99fd5d502b51cd204d58bc929b80a0794836
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-04 10:26:08 -0500

    still working on first modal

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,16 @@
-document.addEventListener('click', function(e) {
-  if(e.target.document.contains.getElementById("customTees")){
-    const src = e.target.document.getElementById("src");
-    console.log(src);
-  }
+const uxModal = document.getElementById('uxModal')
+uxModal.addEventListener('show.bs.modal', event => {
+  // Button that triggered the modal
+  const button = event.relatedTarget
+  // Extract info from data-bs-* attributes
+  const recipient = button.getAttribute('data-bs-whatever')
+  // If necessary, you could initiate an AJAX request here
+  // and then do the updating in a callback.
+  //
+  // Update the modal's content.
+  const modalTitle = uxModal.querySelector('.modal-title')
+  const modalBodyInput = uxModal.querySelector('.modal-body input')
+
+  modalTitle.textContent = `New message to ${recipient}`
+  modalBodyInput.value = recipient
 })
