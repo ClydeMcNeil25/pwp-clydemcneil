1239381b484f6a69875d7fec31debefc5aec5cd6
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-05 00:19:30 -0500

    finished and armed placeholders for modals, still working the youtube video responsiveness

--- public_html/script.js
+++ public_html/script.js
@@ -1,16 +1,15 @@
-const uxModal = document.getElementById('uxModal')
-uxModal.addEventListener('show.bs.modal', event => {
-  // Button that triggered the modal
-  const button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  const recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  const modalTitle = uxModal.querySelector('.modal-title')
-  const modalBodyInput = uxModal.querySelector('.modal-body input')
-
-  modalTitle.textContent = `New message to ${recipient}`
-  modalBodyInput.value = recipient
-})
+// document.addEventListener("click", function (e){
+//   if(e.target.alt.contains("customTees")){
+//     const src = e.target.getAttribute("src");
+//     document.querySelector("")
+//     console.log(src);
+//   }
+// })
+//
+// document.addEventListener("click", function (e){
+//   if(e.target.alt.contains("medicareMel")){
+//     const src = e.target.getAttribute("src");
+//     document.querySelector("")
+//     console.log(src);
+//   }
+// })
