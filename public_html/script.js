8efc80eb44899e89d97eac27fdd8052fd29fb923
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-11 13:52:47 -0500

    still trying to solve the amazing case of the parallax effect

--- public_html/script.js
+++ public_html/script.js
@@ -15,14 +15,14 @@ function autoPlayYouTubeModal() {
 }
 
 //Parallax Effect
-let bg = document.getElementsByClassName("background");
-let mg = document.getElementsByClassName('midground');
-let fg = document.getElementsByClassName('foreground');
+const images = document.querySelectorAll('.images img');
 
-window.addEventListener('scroll', function(){
-  let value = window.scrollY;
-
-  bg.style.top = value * 0.15 + 'px';
-  // mg.style.top = value * 0.25 + 'px';
-  // fg.style.top = value * 0.5 + 'px';
+window.addEventListener('scroll', () => {
+  images[0].style.top = "-" + (window.scrollY / 2.5) + "px";
+  images[1].style.top = "-" + (window.scrollY / 3.5) + "px";
+  images[2].style.top = "-" + (window.scrollY / 4) + "px";
+  images[3].style.top = "-" + (window.scrollY / 5) + "px";
+  images[4].style.top = "-" + (window.scrollY / 6) + "px";
+  images[5].style.top = "-" + (window.scrollY / 7) + "px";
+  images[6].style.top = "-" + (window.scrollY / 9) + "px";
 });
