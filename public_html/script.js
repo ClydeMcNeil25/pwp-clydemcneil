dbd892b0146bdd1be9fc6aa655b4342b8691c93d
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-14 00:11:56 -0500

    started the parallax effect, have to go back to photoshop to separate and provide more layers to pull the effect off correctly

--- public_html/script.js
+++ public_html/script.js
@@ -1,28 +1,29 @@
-
-autoPlayYouTubeModal();
-//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
-function autoPlayYouTubeModal() {
-  let trigger = document.find.id('#motionLabel1');
-  trigger.click(function () {
-    let theModal = $(this).data("target"),
-      videoSRC = $(this).attr("data-theVideo"),
-      videoSRCauto = videoSRC + "?autoplay=1";
-    $(theModal + ' iframe').attr('src', videoSRCauto);
-    $(theModal + ' button.close').click(function () {
-      $(theModal + ' iframe').attr('src', videoSRC);
-    });
-  });
-}
+//
+// autoPlayYouTubeModal();
+// //FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+// function autoPlayYouTubeModal() {
+//   let trigger = document.find.id('#motionLabel1');
+//   trigger.click(function () {
+//     let theModal = $(this).data("target"),
+//       videoSRC = $(this).attr("data-theVideo"),
+//       videoSRCauto = videoSRC + "?autoplay=1";
+//     $(theModal + ' iframe').attr('src', videoSRCauto);
+//     $(theModal + ' button.close').click(function () {
+//       $(theModal + ' iframe').attr('src', videoSRC);
+//     });
+//   });
+// }
 
 //Parallax Effect
-const images = document.querySelectorAll('.images img');
+let foreground = document.getElementById('foreground');
+let midground = document.getElementById('midground');
+let background = document.getElementById('background');
+let welcome = document.getElementById('welcome');
 
-window.addEventListener('scroll', () => {
-  images[0].style.top = "-" + (window.scrollY / 2.5) + "px";
-  images[1].style.top = "-" + (window.scrollY / 3.5) + "px";
-  images[2].style.top = "-" + (window.scrollY / 4) + "px";
-  images[3].style.top = "-" + (window.scrollY / 5) + "px";
-  images[4].style.top = "-" + (window.scrollY / 6) + "px";
-  images[5].style.top = "-" + (window.scrollY / 7) + "px";
-  images[6].style.top = "-" + (window.scrollY / 9) + "px";
-});
+window.addEventListener('scroll', function(){
+  let value = window.scrollY;
+
+  welcome.style.top = value * 0.5 + 'px';
+  midground.style.top = value * 0.25 + 'px';
+  foreground.style.top = value * -0.15 + 'px';
+})
