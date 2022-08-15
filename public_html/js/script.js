11cc0cbdd3e15d1071fa4ca28dcdd24c9a471b45
Clyde McNeil <cmcneil2@cnm.edu>
2022-08-15 11:49:30 -0500

    added to src files

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -11,9 +11,9 @@ window.addEventListener('scroll', function(){
 
   stars.style.left = value * 0.05 + 'px';
   moon.style.left = value * 1.25 + 'px';
-  farground.style.top = value * 0.10 + 'px';
-  background.style.top = value * 0.15 + 'px';
-  midground.style.top = value * 0.20 + 'px';
+  farground.style.top = value * -0.10 + 'px';
+  background.style.top = value * -0.15 + 'px';
+  midground.style.top = value * -0.20 + 'px';
   foreground.style.top = value * -0.15 + 'px';
 });
 
@@ -35,3 +35,60 @@ window.addEventListener('scroll', function(){
 
 
 /* Contact Form */
+$(document).ready(function(){
+  $('#contact').validate({
+    debug: true,
+    errorClass: 'alert alert-danger',
+    ErrorLabelContainer: '#output-area',
+    errorElement: 'div',
+    // rules to define what good and bad inputs
+    // each rule start with the form input element's NAME attribute
+    rules: {
+      firstname: {
+        required: true
+      },
+      lastname: {
+        required: true
+      },
+      email: {
+        email: true,
+        required: true
+      },
+      message: {
+        required: true,
+        maxlength: 1000
+      }
+    },
+    messages: {
+      firstname: {
+        required: 'Name is required.'
+      },
+      lastname: {
+        required: 'Name is required.'
+      },
+      email: {
+        email: 'Please provide a valid email.',
+        required: 'Email is required.'
+      },
+      message: {
+        required: 'A message is required.',
+        maxlength: 'Message must be 1000 characters or less.'
+      }
+    },
+    submitHandler: (form) => {
+      $('#contact').ajaxSubmit({
+        type: 'POST',
+        url: $('contact').attr('action'),
+        success: (ajaxOutput) => {
+          $('#output-area').css('display', '')
+          $('#output-area').html(ajaxOutput)
+
+          if($('.alert-success') >= 1){
+            $('#contact')[0].reset()
+          }
+        }
+      })
+    }
+
+  })
+})
